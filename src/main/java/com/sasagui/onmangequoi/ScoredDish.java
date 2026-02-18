4d3a889a0a8157a9ba706affea27cd7e18f8d837
Guillaume Lucazeau <glucazeau@gmail.com>
2026-02-18 20:26:56 +0100

    test: test ScoredDish compareTo

--- src/main/java/com/sasagui/onmangequoi/ScoredDish.java
+++ src/main/java/com/sasagui/onmangequoi/ScoredDish.java
@@ -6,6 +6,7 @@ import lombok.*;
 @Getter
 @ToString(onlyExplicitlyIncluded = true)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
 public class ScoredDish implements Comparable<ScoredDish> {
 
     @ToString.Include
@@ -16,13 +17,8 @@ public class ScoredDish implements Comparable<ScoredDish> {
     @ToString.Include
     private int score;
 
-    public ScoredDish(final Dish dish) {
-        this.dish = dish;
-        this.score = 0;
-    }
-
     @Override
     public int compareTo(@NotNull ScoredDish other) {
-        return Integer.compare(other.score, this.score);
+        return Integer.compare(this.score, other.score);
     }
 }
