3b23d0e4905760520abef4c9376269d64e247b69
Guillaume Lucazeau <glucazeau@gmail.com>
2026-02-23 10:29:02 +0100

    feat: add Day initializer

--- src/main/java/com/sasagui/onmangequoi/Day.java
+++ src/main/java/com/sasagui/onmangequoi/Day.java
@@ -1,18 +1,24 @@
 package com.sasagui.onmangequoi;
 
 import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Day {
 
     private final DayOfWeek dayOfWeek;
 
-    private final LocalDate date;
-
     private final List<Meal> meals;
+
+    public static Day dinner(DayOfWeek dayOfWeek) {
+        return new Day(dayOfWeek, List.of(new Meal(MealType.DINNER)));
+    }
+
+    public static Day lunchAndDinner(DayOfWeek dayOfWeek) {
+        return new Day(dayOfWeek, List.of(new Meal(MealType.LUNCH), new Meal(MealType.DINNER)));
+    }
 }
