9131bb74a86b6698592489ed6ae075f7d64f771e
Guillaume Lucazeau <glucazeau@gmail.com>
2026-03-11 09:42:43 +0100

    feat(dish): add unique constraint on dish label
    Fixes #43

--- src/test/groovy/com/sasagui/onmangequoi/dish/DishIntegrationSpec.groovy
+++ src/test/groovy/com/sasagui/onmangequoi/dish/DishIntegrationSpec.groovy
@@ -21,7 +21,7 @@ class DishIntegrationSpec extends IntegrationSpec {
                 .andExpect(jsonPath("\$", hasSize(29)))
     }
 
-    def "POST /dishes -new dish body sent - returns HTTP 201"() {
+    def "POST /dishes - new dish body sent - returns HTTP 201"() {
         when:
         def response = mvc.perform(post("/dishes")
                 .contentType(MediaType.APPLICATION_JSON).content("""{
@@ -36,7 +36,7 @@ class DishIntegrationSpec extends IntegrationSpec {
         response.andExpect(status().isCreated())
     }
 
-    def "GET /dishes - new dish has been aded"() {
+    def "GET /dishes - new dish has been added"() {
         when:
         def response = mvc.perform(get("/dishes")
                 .contentType(MediaType.APPLICATION_JSON))
@@ -45,4 +45,20 @@ class DishIntegrationSpec extends IntegrationSpec {
         response.andExpect(status().isOk())
                 .andExpect(jsonPath("\$", hasSize(30)))
     }
+
+    def "POST /dishes -new dish body sent with same label - returns HTTP 409"() {
+        when:
+        def response = mvc.perform(post("/dishes")
+                .contentType(MediaType.APPLICATION_JSON).content("""{
+                            "label": "Poulet frites",
+                            "slow": true,
+                            "quick": false,
+                            "fromRestaurant": false,
+                            "vegan": false
+                        }"""))
+
+        then:
+        response.andExpect(status().isConflict())
+    }
+
 }
